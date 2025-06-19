aa49cae0e60a8f7cf3ec5a4c9ba73984b32930bf
anil gummula <anilgummula2060@gmail.com>
2025-06-19 08:39:27 +0530

    upadetd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
   return (
     <div className='flex items-center justify-between gap-4 py-3 mt-20'>
         <img width={24} src={assets.logo_icon} alt="" />
-        <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @anilgummula.dev | All right reserved.</p>
-        <div className='flex gap-2.5'>
+        <p className='text-white flex-1 border-l border-gray-400 pl-4 text-sm max-sm:hidden'>Copyright @anilgummula.dev | All right reserved.</p>
+        <div className='flex gap-2.5 text-white'>
             <img width={35} src={assets.facebook_icon} alt="" />
             <img width={35} src={assets.twitter_icon} alt="" />
             <img width={35} src={assets.instagram_icon} alt="" />
