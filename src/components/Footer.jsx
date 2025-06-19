855b74785ce91680696435638062e96bd887d2b5
anil gummula <anilgummula2060@gmail.com>
2025-06-19 11:05:29 +0530

    upadetd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,11 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div className='flex items-center justify-between gap-4 py-3 mt-20'>
-        <img width={24} src={assets.logo_icon} alt="" />
-        <p className='text-white flex-1 border-l border-gray-400 pl-4 text-sm max-sm:hidden'>Copyright @anilgummula.dev | All right reserved.</p>
-        <div className='flex gap-2.5 text-white'>
-            <img width={35} src={assets.facebook_icon} alt="" />
-            <img width={35} src={assets.twitter_icon} alt="" />
-            <img width={35} src={assets.instagram_icon} alt="" />
-        </div>
+    <div className='flex justify-center mx-auto'>
+        <a className='flex items-center justify-between gap-4 py-3 mt-20 hover:pointer' href="https://anilgummula.netlify.app/" target='_blank'>
+            <img width={24} src={assets.logo_icon} alt="" />
+            <p className='text-white flex-1 border-l border-gray-400 pl-4 text-sm max-sm:hidden'>Copyright @anilgummula.dev | All right reserved.</p>
+        </a>
     </div>
   )
 }
